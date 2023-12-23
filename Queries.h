24f207995ea3cf0f64203cb25a0bebd9e701a718
andreyv2048 <andreyv2048@gmail.com>
2023-12-23 10:29:09 +0500

    gost

---
+++ Queries.h
@@ -0,0 +1,9 @@
+//---------------------------------------------------------------------------
+
+#ifndef QueriesH
+#define QueriesH
+#include <list>
+#include <System.hpp>
+//---------------------------------------------------------------------------
+std::list<UnicodeString>GetGost();
+#endif
